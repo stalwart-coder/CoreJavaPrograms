8dcd5ef9cbe20844c51c396e09f39c6f86836c85
Bipin <logic.legend123@gmail.com>
2024-12-29 07:36:18 +0530

    first commit

---
+++ 3_Encapsulation/src/LaunchEncapsulation1.java
@@ -0,0 +1,34 @@
+//in java encapsulation whole purpose is to provide data security using getters and setters ,data hiding ,manipulating the data.
+class House{
+	private int price;
+	private String address;
+	//setters
+	void setPrice(int price) {
+		this.price=price;
+	}
+	void setAddress(String address) {
+		this.address=address;
+	}
+	//getters
+	int getPrice() {
+		return price;
+	}
+	String getAddress() {
+		return address;
+	}
+}
+public class LaunchEncapsulation1 {
+
+	public static void main(String[] args) {
+		House h=new House();
+		h.setAddress("ratanpur");
+		String addr=h.getAddress();
+		System.out.println(addr);
+		h.setPrice(10000);
+		int q=h.getPrice();
+		System.out.println(q);
+	
+		
+	}
+
+}
